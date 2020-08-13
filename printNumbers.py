8505fbbcbf1bc1bc12ae707684e57f8f6ef912db
vegimohith <mohith.vegi@dailyhunt.in>
2020-08-13 16:02:47 +0530

    Print Numbers

---
+++ printNumbers.py
@@ -0,0 +1,6 @@
+# https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/print-the-numbers/
+
+N = int(input())
+numbers = input()
+
+print(numbers)
