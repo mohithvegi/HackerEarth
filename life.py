4e6ad9923ffc1f97ae894a1bbbc644e7e9ed48ae
vegimohith <mohith.vegi@dailyhunt.in>
2020-08-14 18:53:23 +0530

    Life, the Universe, and Everything

---
+++ life.py
@@ -0,0 +1,12 @@
+# https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/life-the-universe-and-everything/
+
+try:
+    while(True):
+        N = int(input())
+        if(N==42):
+            break
+        else:
+            print(N)
+
+except:
+    print("Error.....")
