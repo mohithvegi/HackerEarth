a9a029bbe099f6b3d63a0b37ea09674f532e8710
vegimohith <mohith.vegi@dailyhunt.in>
2020-08-14 19:52:29 +0530

    Ladderophilia

---
+++ ladderophilia.py
@@ -0,0 +1,11 @@
+# https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/ladderophilia/
+
+N = int(input())
+
+for n in range(N):
+    print("*   *")
+    print("*   *")
+    print("*****")
+
+print("*   *")
+print("*   *")
